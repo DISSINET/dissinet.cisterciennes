4899927cdf44ef72a0ebccb08e90a348676655a4
adammertel <mertel.adam@gmail.com>
2018-11-16 09:52:22 +0100

    parsing values

--- scrap.js
+++ scrap.js
@@ -1,5 +1,8 @@
 var request = require('request')
 var cheerio = require('cheerio')
+var csv = require('ya-csv')
+
+var writer = csv.createCsvFileWriter('out.csv', { flags: '' })
 
 const url =
   'https://fr.wikipedia.org/wiki/Liste_d%27abbayes_cisterciennes_de_France'
@@ -8,13 +11,44 @@ request(url, function (err, resp, html) {
   if (!err) {
     const $ = cheerio.load(html)
     $('table.wikitable.sortable tbody').find('tr').map((ri, row) => {
-      if (ri === 1) {
+      if (ri < 10) {
         // row
-        console.log($(row).html())
+
+        const rowValues = []
         $(row).find('td').map((ci, column) => {
-          console.log($(column).html())
+          // name and link
+          if (ci === 1) {
+            // name
+            rowValues.push($(column).text())
+
+            // link
+            rowValues.push(
+              'https://fr.wikipedia.org' + $(column).find('a').attr('href')
+            )
+          } else if (ci === 2) {
+            // lat lng
+            rowValues.push($(column).find('a').data('lat'))
+            rowValues.push($(column).find('a').data('lon'))
+          } else if (ci === 7 || ci === 8) {
+            rowValues.push(
+              $(column)
+                .html()
+                .split('<br>')
+                .map(t => {
+                  return t
+                    .replace('<b>', '')
+                    .replace('</b>', '')
+                    .replace('-', '?')
+                })
+                .join('-')
+            )
+          } else {
+            rowValues.push($(column).text())
+          }
         })
+        writer.writeRecord(rowValues)
       }
     })
   }
+  writer.writeStream.end()
 })

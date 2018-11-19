0b38ed939da53bf0f5a935896572a8b529977676
adammertel <mertel.adam@gmail.com>
2018-11-19 12:08:14 +0100

    labels tms

--- app/index.ts
+++ app/index.ts
@@ -53,6 +53,17 @@ var init = () => {
       className: 'map-base-layer-awmc'
     }
   ).addTo(map);
+  L.tileLayer(
+    'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-labels/{z}/{x}/{y}{r}.{ext}',
+    {
+      attribution:
+        'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      subdomains: 'abcd',
+      minZoom: 0,
+      maxZoom: 20,
+      ext: 'png'
+    }
+  ).addTo(map);
 
   const genderScore = {
     male: 0,

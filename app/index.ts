ddbbb02c7c46ab259afed553c795f7b29e65bfdb
adammertel <mertel.adam@gmail.com>
2018-11-19 10:46:37 +0100

    preparing data

--- app/index.ts
+++ app/index.ts
@@ -1,15 +1,62 @@
 import * as monasteriesJSON from './data/monasteries.json';
 import * as ordersJSON from './data/orders.json';
 
-import leaflet from 'leaflet';
+import './main.scss';
+import './../node_modules/leaflet/dist/leaflet.css';
+
+import L from 'leaflet';
 
 var monasteries = Object.values(monasteriesJSON);
 var orders = Object.values(ordersJSON);
 
 console.log(monasteries);
 
+if (document.body) {
+  document.body.innerHTML = '';
+}
+
+var data = [];
+var map = false;
 var init = () => {
-  const mapEl = document.getElementById('map');
+  // crate map div
+  const mapEl = document.createElement('div');
+  mapEl.setAttribute('id', 'map');
+  document.body.appendChild(mapEl);
+
+  data = prepareData();
+  console.log(data);
+
+  // initialise map
+  map = L.map('map', {
+    center: [47, 2],
+    zoom: 7
+  });
+
+  // add a awmc tilelayer
+  L.tileLayer(
+    'http://a.tiles.mapbox.com/v3/isawnyu.map-knmctlkh/{z}/{x}/{y}.png',
+    {
+      attribution: "<a href='http://awmc.unc.edu/wordpress/'>awmc</a>",
+      className: 'map-base-layer-awmc'
+    }
+  ).addTo(map);
+};
+
+var prepareData = (): Array<Object> => {
+  monasteries.forEach(monastery => (monastery.orders = []));
+  orders.forEach(order => {
+    const orderMonasteryId = order.monastery_id;
+
+    const monastery = monasteries.find(
+      monastery => monastery.id === orderMonasteryId
+    );
+
+    if (monastery) {
+      monastery.orders.push(order);
+    }
+  });
+
+  return monasteries;
 };
 
 init();
